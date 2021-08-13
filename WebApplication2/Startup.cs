6432f1e770a5d3e0225ab1a2d357f64e3b639b60
NAGENDRA CHINNAM <nagendra571@gmail.com>
2021-08-13 11:39:10 -0400

    commmit

--- WebApplication2/Startup.cs
+++ WebApplication2/Startup.cs
@@ -24,6 +24,7 @@ namespace WebApplication2
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddKendo();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
